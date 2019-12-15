0bf2f65b502a6bcc457526dd698489ef27b07a72
Ioannis Mittas <jmittas@gmail.com>
2019-12-15 14:37:07 +0100

    Add skeleton for BaseUseCase

---
+++ domain/src/main/java/com/mittas/bitcoingraph/domain/usecases/BaseUseCase.kt
@@ -0,0 +1,7 @@
+package com.mittas.bitcoingraph.domain.usecases
+
+import javax.inject.Inject
+
+//abstract class BaseUseCase @Inject constructor(val workExecutionThread: WorkExecutionThread,
+//                                               val postExecutionThread: PostExecutionThread)
+
