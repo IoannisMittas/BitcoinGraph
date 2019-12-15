b5f4fc659f4b0f741fe53a31ceda4cea00f587dc
Ioannis Mittas <jmittas@gmail.com>
2019-12-15 23:19:27 +0100

    Add empty GraphActivity

---
+++ app/src/main/java/com/mittas/bitcoingraph/ui/screen/graph/GraphActivity.kt
@@ -0,0 +1,14 @@
+package com.mittas.bitcoingraph.ui.screen.graph
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.mittas.bitcoingraph.R
+
+class GraphActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_graph)
+    }
+
+}
