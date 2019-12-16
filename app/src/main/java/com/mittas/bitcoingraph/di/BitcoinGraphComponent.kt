13d20524589a6f8d24a53939f0c5098dc75a7033
Ioannis Mittas <jmittas@gmail.com>
2019-12-16 18:05:15 +0100

    Add ViewModelsModule in order to be able to inject ViewModels constructors

--- app/src/main/java/com/mittas/bitcoingraph/di/BitcoinGraphComponent.kt
+++ app/src/main/java/com/mittas/bitcoingraph/di/BitcoinGraphComponent.kt
@@ -4,7 +4,7 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [SchedulersModule::class, RepositoriesModule::class, ApiModule::class])
+@Component(modules = [SchedulersModule::class, RepositoriesModule::class, ApiModule::class, ViewModelsModule::class])
 interface BitcoinGraphComponent {
 
 }
--- app/src/main/java/com/mittas/bitcoingraph/ui/screen/graph/GraphViewModel.kt
+++ app/src/main/java/com/mittas/bitcoingraph/ui/screen/graph/GraphViewModel.kt
@@ -4,8 +4,11 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.mittas.bitcoingraph.domain.entity.charts.MarketPriceChart
+import com.mittas.bitcoingraph.domain.usecases.charts.GetMarketPriceChartUseCase
+import javax.inject.Inject
 
-class GraphViewModel : ViewModel() {
+class GraphViewModel @Inject constructor(private val getMarketPriceChartUseCase: GetMarketPriceChartUseCase) :
+    ViewModel() {
 
     private val _bitcoinPriceChart: MutableLiveData<MarketPriceChart> = MutableLiveData()
     val bitcoinPriceChart: LiveData<MarketPriceChart>
@@ -15,8 +18,9 @@ class GraphViewModel : ViewModel() {
 
     }
 
+    private fun fetchBitcoinPriceChart() {
 
-// todo add viewmodel factory???
 
+    }
 
 }
